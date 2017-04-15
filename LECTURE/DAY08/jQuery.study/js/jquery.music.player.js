036907fe30a8086a8759caa212158374af6457c9
Jee Hoon <yamoo9@naver.com>
2017-04-15 19:33:38 +0900

    SVG 애니메이션 설명

--- LECTURE/DAY08/jQuery.study/js/jquery.music.player.js
+++ LECTURE/DAY08/jQuery.study/js/jquery.music.player.js
@@ -1,5 +1,6 @@
 /*! jquery.music.player.js © yamoo9.net, 2017 */
 
+// 모델 데이터 (배열)
 var coverList = [
   {
     src: "001.EdSheeran-ShapeOfYou.jpg",
@@ -43,6 +44,7 @@ var coverList = [
   }
 ];
 
+// jQuery 코드(캡슐화)
 jQuery(function($){
 
   /////////////
@@ -73,17 +75,80 @@ jQuery(function($){
   // console.log('$m_coverlist:', $m_coverlist);
 
   // JavaScript 스타일 코드
-  for ( var i=0, l=coverList.length; i<l; ++i ) {
-    var cover = coverList[i];
-    var $cover = $([
+  // for ( var i=0, l=coverList.length; i<l; ++i ) {
+  //   var cover = coverList[i];
+  //   var $cover = $([
+  //     '<li class="music-cover-item">',
+  //       '<img class="music-cover is-rwd" src="../../media/cover/'+ cover.src +'" alt="'+ cover.alt +'">',
+  //     '</li>'
+  //   ].join('')).appendTo($m_coverlist);
+  //   // console.log($cover.html());
+  //   // $m_coverlist.append($cover);
+  // }
+
+  // jQuery 스타일 코드
+  // Utility(Static) Methods
+  // http://api.jquery.com/jQuery.each/
+  // $.each(coverList, function(index, cover) {
+  //   $([
+  //     '<li class="music-cover-item">',
+  //       '<img class="music-cover is-rwd" src="../../media/cover/'+ cover.src +'" alt="'+ cover.alt +'">',
+  //     '</li>'
+  //   ].join('')).appendTo($m_coverlist);
+  // });
+
+  /////////////
+  // STEP 3. //
+  /////////////
+  // var controls = [
+  //   '<div class="player-indicators"></div>',
+  //   '<div class="player-controller">',
+  //     '<button type="button" class="button is-button is-prev" aria-label="PREV"></button>',
+  //     '<button type="button" class="button is-button is-next" aria-label="NEXT"></button>',
+  //   '</div>'
+  // ].join('');
+
+  // $m_player.append(controls);
+
+  $([
+    '<div class="player-indicators"></div>',
+    '<div class="player-controller">',
+      '<button type="button" class="button is-button is-prev" aria-label="PREV"></button>',
+      '<button type="button" class="button is-button is-next" aria-label="NEXT"></button>',
+    '</div>'
+  ].join('')).appendTo($m_player);
+
+  var $m_indicators = $('.player-indicators', $m_player);
+
+  var distance = $m_player.width();
+
+  // jQuery 스타일 반복문
+  $.each(coverList, function(index, cover) {
+
+    // 커버 이미지 삽입
+    $([
       '<li class="music-cover-item">',
         '<img class="music-cover is-rwd" src="../../media/cover/'+ cover.src +'" alt="'+ cover.alt +'">',
       '</li>'
     ].join('')).appendTo($m_coverlist);
-    // console.log($cover.html());
-    // $m_coverlist.append($cover);
-  }
 
-  // jQuery 스타일 코드
+    // 인디케이터 삽입
+    $m_indicators.append('<a href role="tab" class="indicator" aria-label="'+cover.alt+'"></a>');
+
+    $m_indicators_tabs = $m_indicators.find('.indicator');
+
+    $m_indicators_tabs.eq(index).on('click', function(){
+      // console.log(this.find); // DOM Object
+      // console.log( $(this).find ); // jQuery {}
+      var $tab = $(this);
+      var index = $tab.index();
+      $tab.parent().find('.is-active').removeClass('is-active');
+      $tab.addClass('is-active');
+      // jQuery객체.css('속성', '값')
+      $m_coverlist.css('transform', 'translateX(' + index * -1 * distance + 'px)');
+      return false;
+    });
+
+  });
 
 });
