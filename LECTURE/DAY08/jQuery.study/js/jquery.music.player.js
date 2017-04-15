38ec1e5bf3cd02a6c63da20e11709e1f0018f8cd
Jee Hoon <yamoo9@naver.com>
2017-04-15 18:04:07 +0900

    jQuery 뮤직 플레이어 실습 준비

---
+++ LECTURE/DAY08/jQuery.study/js/jquery.music.player.js
@@ -0,0 +1,48 @@
+/*! jquery.music.player.js © yamoo9.net, 2017 */
+
+var coverList = [
+  {
+    src: "001.EdSheeran-ShapeOfYou.jpg",
+    alt: "Ed Sheeran - Shape Of You"
+  },
+  {
+    src: "002.TheChainsmokers&Halsey-Closer.jpg",
+    alt: "The Chainsmokers & Halsey - Closer"
+  },
+  {
+    src: "003.Zayn&TaylorSwift-IDon'tWannaLiveForever(FiftyShadesDarker).jpg",
+    alt: "Zayn & TaylorSwift - I Don't Wanna Live Forever (Fifty Shades Darker)"
+  },
+  {
+    src: "004.KatyPerry&SkipMarley-ChainedToTheRhythm.jpg",
+    alt: "Katy Perry & Skip Marley - Chained To The Rhythm"
+  },
+  {
+    src: "005.Migos&LilUziVert-BadAndBoujee.jpg",
+    alt: "Migos & Lil Uzi Vert - Bad And Boujee"
+  },
+  {
+    src: "006.TheChainsmokers-Paris.jpg",
+    alt: "The Chainsmokers - Paris"
+  },
+  {
+    src: "007.Rihanna-LoveOnTheBrain.jpg",
+    alt: "Rihanna - Love On The Brain"
+  },
+  {
+    src: "008.BrunoMars-That'sWhatILike.jpg",
+    alt: "Bruno Mars - That's What I Like"
+  },
+  {
+    src: "009.BigSean-BounceBack.jpg",
+    alt: "Big Sean - Bounce Back"
+  },
+  {
+    src: "010.MachineGunKelly&CamilaCabello-BadThings.jpg",
+    alt: "Machine Gun Kelly & Camila Cabello - Bad Things"
+  }
+];
+
+jQuery(function($){
+
+});
