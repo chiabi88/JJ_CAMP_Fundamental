05a71ced8904c448d1efe0d4e4fe5a18324c6efe
Jee Hoon <yamoo9@naver.com>
2017-04-22 18:01:18 +0900

    jQuery 데스크탑 카트 메뉴 버튼 컨트롤 코드 추가

--- LECTURE/DAY09/UI/offcanvas-menus/js/offcanvas-menus.js
+++ LECTURE/DAY09/UI/offcanvas-menus/js/offcanvas-menus.js
@@ -1,1 +1,37 @@
 /*! offcanvas-menus.js © yamoo9.net, 2017 */
+jQuery(function($){
+  // 제어하고자 하는 문서 요소를 찾아
+  // jQuery 객체화 시킨다.
+
+  // 모바일 뷰에서만 보이는 햄버거 토글 버튼
+  var $m_button    = $('.y9-hamburger-menu a');
+  // 글로벌 내비게이션 바
+  var $gnb         = $('.main-nav');
+  // 카트 메뉴 토글 버튼
+  var $cart_button = $('.y9-cart-trigger a');
+  // 카트 메뉴
+  var $cart_menu   = $('.y9-cart');
+  // 딤
+  var $dim         = $('.y9-shadow-layer');
+
+  /**
+   * --------------------------------
+   * 이벤트 바인딩 <-> 핸들러
+   */
+  $cart_button.on('click', toggleCartMenu);
+
+  function toggleCartMenu() {
+    // console.log('toggleCartMenu');
+    // 비즈니스 로직(구현할 코드)
+    // 카트 메뉴를 밖에서 안으로 이동시킨다.
+    if ( $cart_menu.hasClass('out-in') ) {
+      $dim.hide();
+      $cart_menu.removeClass('out-in');
+    } else {
+      // 딤처리를 한다.
+      $dim.show();
+      $cart_menu.addClass('out-in');
+    }
+  }
+
+});
